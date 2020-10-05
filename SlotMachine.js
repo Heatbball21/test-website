ed12032af85f3fd6c246bc14fc784cd23663ec7f
Jackson <jjoblinske@gmail.com>
2020-10-04 19:54:57 -0500

    test

--- SlotMachine.js
+++ SlotMachine.js
@@ -125,7 +125,7 @@ function highLow(){
 
 function getWins(arr1){
 
-  holder = {document.getElementById("[0]"), document.getElementById("[1]"), document.getElementById("[2]")}
+  holder = {document.getElementById("[0]"), document.getElementById("[1]"), document.getElementById("[2]")};
 
   document.getElementById("winningNumbers").innerHTML = arr1[0] + arr1[1] + arr1[2];
 
